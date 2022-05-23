e205551b624cbd79204de98a5e62452921cb044f
Nayoung Gu <skdud000322@naver.com>
2022-05-23 18:16:17 +0900

    JS 파일 코드 정리 및 스크롤 기능 추가

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const goBtn = resultCont.querySelector(".go-btn");
 const goModal = document.querySelector(".go-modal");
 const shareBtn = resultCont.querySelector(".share-btn");
 const exitBtn = goModal.querySelector(".exit-btn");
+const mediaQueryWeb = window.matchMedia("(min-width: 400px)");
+const mediaQueryMobile = window.matchMedia("(max-width: 400px)");
 
-function handleSubmit(event) {
+function showResult(event) {
   const resultText = resultCont.querySelector("#resultText");
   const resultDays = resultCont.querySelector("#resultDays");
 
@@ -21,22 +23,33 @@ function handleSubmit(event) {
   resultDays.textContent = Math.floor(10000 / parseInt(hourInput.value));
 }
 
-function handleLoad() {
+function showLoading() {
   resultCont.classList.add("hidden");
-  if (
-    subjectInput.value.length !== 0 &&
-    hourInput.value.length !== 0 &&
-    hourInput.value <= 24
-  ) {
+  // 입력 유무 확인
+  if (subjectInput.value.length !== 0 && hourInput.value.length !== 0) {
+    // 로딩 시계 보여줬다가 숨기기
     loadingClock.classList.remove("hidden");
     setTimeout(() => {
       loadingClock.classList.add("hidden");
     }, 1500);
+    // 로딩 시계 및 결과 부분으로 스크롤 이동
+    window.scrollTo({ top: 800, behavior: "smooth" });
   }
 }
 
 function handleGoModal() {
   goModal.classList.remove("hidden");
+
+  // 웹, 모바일에 따라 모달창 부분으로 스크롤 이동
+  const location = goModal.offsetTop;
+  if (mediaQueryWeb.matches) {
+    window.scrollTo({ top: location + 350, behavior: "smooth" });
+  }
+  if (mediaQueryMobile.matches) {
+    window.scrollTo({ top: location, behavior: "smooth" });
+  }
+
+  // 종료 버튼 클릭시 모달창 숨기기
   exitBtn.addEventListener("click", () => {
     goModal.classList.add("hidden");
   });
@@ -55,7 +68,7 @@ function handleShareBtn() {
   window.alert("링크가 복사되었습니다.");
 }
 
-form.addEventListener("submit", handleSubmit);
-calcBtn.addEventListener("click", handleLoad);
+form.addEventListener("submit", showResult);
+calcBtn.addEventListener("click", showLoading);
 goBtn.addEventListener("click", handleGoModal);
 shareBtn.addEventListener("click", handleShareBtn);
